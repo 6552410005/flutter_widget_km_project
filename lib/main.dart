a024e7aed5d1fbff8dd3c461dd7bddba3e92c429
6552410005 <s6552410005@sau.ac.th>
2024-04-01 10:39:18 +0700

    UpdateV1.1

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_widget_kn_project/views/km_bnb_01_ui.dart';
 import 'package:flutter_widget_kn_project/views/km_bnb_02_ui.dart';
 import 'package:flutter_widget_kn_project/views/km_bnb_03_ui.dart';
 import 'package:flutter_widget_kn_project/views/km_button_ui.dart';
+import 'package:flutter_widget_kn_project/views/km_cameragallery_ui.dart';
 import 'package:flutter_widget_kn_project/views/km_checkbox_ui.dart';
 import 'package:flutter_widget_kn_project/views/km_dropdown_ui.dart';
 import 'package:flutter_widget_kn_project/views/km_radio_ui.dart';
@@ -30,7 +31,7 @@ class _MainAppState extends State<MainApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: KmDropdownUI(),
+      home: KmCameraGalleryUI(),
       theme: ThemeData(
         textTheme: GoogleFonts.kanitTextTheme(
           Theme.of(context).textTheme,
